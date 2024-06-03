873f8754b4a001df87b5a11434e6ae2014c4ba5b
learntripathi <learntripathi@gmail.com>
2024-06-03 06:06:08 +0530

    Update modelApi.py
    secured spi key

--- modelApi.py
+++ modelApi.py
@@ -11,9 +11,9 @@ from utilities import API_KEY,DB_PATH,META_DATA_PATH
 def modelApi():
     meta = pd.read_csv(META_DATA_PATH)
     db = SQLDatabase.from_uri(DB_PATH)
-    llm = OpenAI(temperature=0, verbose=False,api_key=API_KEY )
+    llm = OpenAI(temperature=0, verbose=False,api_key= st.secrets['API_KEY'] )
     db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=False)
-    chain = create_sql_query_chain(ChatOpenAI(temperature=0,api_key= API_KEY), db)
+    chain = create_sql_query_chain(ChatOpenAI(temperature=0,api_key=  st.secrets['API_KEY']), db)
 
     return db_chain,chain,meta
 
